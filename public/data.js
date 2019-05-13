dfcbc1a0e65e36d49f1ae3f89b7d15e4b757291d
krasch <dev@krasch.io>
2019-05-13 10:42:00 +0200

    Connect answer buttons and countdown timer

--- public/data.js
+++ public/data.js
@@ -1,5 +1,4 @@
 const DATA = [
         {"image": "https://images.pexels.com/photos/56832/road-asphalt-space-sky-56832.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-         "answers": [{"name": "Strasse", "correct": true},
-                     {"name": "Haus", "correct": false},
-                     {"name": "Mensch", "correct": false}]}]
+         "answers": ["Strasse", "Haus", "Mensch"],
+         "correct": 0}];
