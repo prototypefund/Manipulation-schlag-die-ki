95cca9b5c51dd7506441fce6cd447eadb3ac787e
krasch <dev@krasch.io>
2019-05-15 18:03:07 +0200

    Add score display

--- public/data.js
+++ public/data.js
@@ -1,4 +1,4 @@
 const DATA = [
         {"image": "https://images.pexels.com/photos/56832/road-asphalt-space-sky-56832.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-         "answers": ["Strasse", "Haus", "Mensch"],
+         "answers": ["Straße", "Haus", "Mensch"],
          "correct": 0}];
