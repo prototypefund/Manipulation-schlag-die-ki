ad3caa7b10e67980577a469721c7fac8f364b6a1
krasch <dev@krasch.io>
2019-05-17 22:57:58 +0200

    Intro animation :-)

--- public/text.js
+++
@@ -1,2 +0,0 @@
-const TEXT = {};
-TEXT.intro = "Hier erscheint gleich ein Bild. Welches Verkehrszeichen ist zu sehen?";
--- public/util.js
+++ public/util.js
@@ -1,3 +1,7 @@
+const RESULT = {"CORRECT": "correct", "INCORRECT": "incorrect"};
+const STATUS = {"NOT_STARTED": "not_started", "WAITING": "waiting", "FINISHED": "finished"};
+
+
 // riot mount allows mounting to elements with some given ID or class
 // since there might be multiple elements with some class, mounting returns an array
 // since we always to ID and never to class, we are only ever interested in the first item of that array
@@ -8,5 +12,28 @@ function mountToId(id, tagName, opts) {
     return mounted[0];
 }
 
-const RESULT = {"CORRECT": "correct", "INCORRECT": "incorrect"};
-const STATUS = {"NOT_STARTED": "not_started", "WAITING": "waiting", "FINISHED": "finished"};
+function Timer(stepLength, totalSteps, onStep, onTimeout){
+    let obj = {};
+
+    obj.steps = 0;
+    obj.timer = null;
+
+    obj.start = function() {
+        obj.timer = setInterval(obj.onStep, stepLength);
+    };
+
+    obj.onStep = function() {
+        obj.steps += 1;
+
+        if (onStep instanceof Function)
+            onStep();
+
+        if (obj.steps == totalSteps){
+            clearInterval(obj.timer);
+            if (onTimeout instanceof Function)
+                onTimeout();
+        }
+    };
+
+    return obj;
+}
