1e20a3e9dcb5d2aafe3a3ac68cbcea00db8c5c93
krasch <dev@krasch.io>
2019-05-21 17:32:47 +0200

    Make work in firefox

--- public/scripts/util.js
+++ public/scripts/util.js
@@ -1,16 +1,4 @@
-const RESULT = {"CORRECT": "correct", "INCORRECT": "incorrect"};
-const STATUS = {"NOT_STARTED": "not_started", "WAITING": "waiting", "FINISHED": "finished"};
 
-
-// riot mount allows mounting to elements with some given ID or class
-// since there might be multiple elements with some class, mounting returns an array
-// since we always to ID and never to class, we are only ever interested in the first item of that array
-// -> let's make this explicit
-function mountToId(id, tagName, opts) {
-    let selector = "#" + id;
-    let mounted = riot.mount(selector, tagName, opts);
-    return mounted[0];
-}
 
 function Timer(stepLength, totalSteps, onStep, onTimeout){
     let obj = {};
