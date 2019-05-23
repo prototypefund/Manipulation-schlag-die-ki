a0f8ab655663d756f04ba536acad83f9f2f2ec9a
krasch <dev@krasch.io>
2019-05-23 16:35:38 +0200

    Integrate tensorflow.js

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,10 +1,10 @@
 const DATA = [
-        {"image": "https://images.pexels.com/photos/56832/road-asphalt-space-sky-56832.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-         "answers": ["Straße", "Haus", "Mensch"],
+        {"image": "https://www.fodors.com/wp-content/uploads/2018/04/National-Seashores-Cape-Cod-National-Seashore-Massachusetts.jpg",
+         "answers": ["Strand", "Haus", "Mensch"],
          "correct": 0},
         {"image": "https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg",
             "answers": ["Hund", "Maus", "Katze"],
             "correct": 2},
-        {"image": "https://media.holzpiloten.de/catalog/product/663/8a7e8/tisch-mit-schweizer-kante-buche.jpg?1526391016",
-            "answers": ["Tisch", "Stuhl", "Fahrrad"],
+        {"image": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Motorboat_at_Kankaria_lake.JPG",
+            "answers": ["Boot", "Stuhl", "Fahrrad"],
             "correct": 0}];
